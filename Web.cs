0430a870ca7f9054bf6ca1c51d8b187945ac4c14
psn2706 <sergei.petrov.com@mail.ru>
2021-01-09 12:36:56 +0300

    all errors start with !

--- Web.cs
+++ Web.cs
@@ -10,7 +10,7 @@ class Web
     /// <summary>
     /// Инициализация веб-клиента. Установка соединения с сервером.
     /// </summary>
-    public static void Init()
+    public static string Init()
     {
         try
         {
@@ -24,12 +24,9 @@ class Web
             var con = new ConnectionInfo(ip, name, methods.ToArray());
             client = new SshClient(con);
             client.Connect();
+            return "0";
         }
-        catch
-        {
-            // Напиши "Проверьте подключение к интернету и нажмите ОК", после нажатия ОК:
-            // return Init();
-        }
+        catch { return "!con"; }
     }
     static string RunCommand(string str)
     {
@@ -39,12 +36,7 @@ class Web
             Thread.Sleep(220);
             return ans;
         }
-        catch
-        {
-            // Напиши "Проверьте подключение к интернету и нажмите ОК", после нажатия ОК:
-            // return RunCommand(str);
-        }
-        return "-1";
+        catch { return "!con"; }
     }
     /// <summary>
     /// Создание комнаты.
@@ -59,7 +51,8 @@ class Web
             if (Room.Contains(c.ToString()))
                 return $"Неверный формат ввода, не используйте знаки {spec}";
 
-        if (RunCommand($"if [ -e rooms/{Room}/ ]; then echo -n 'Exists'; fi") == "Exists")
+        string res = RunCommand($"if [ -e rooms/{Room}/ ]; then echo -n 'Exists'; fi"); if (res == "!con") return res;
+        if (res == "Exists")
             return "Комната с таким названием уже существует";
         else
         {
@@ -69,11 +62,12 @@ class Web
                 cmd3 = $"echo -n 'Free' > rooms/{Room}/roomStatus",
                 cmd4 = $"echo -n '{RoomPlayers}' > rooms/{Room}/roomPlayers";
 
-            RunCommand($"{cmd1} && {cmd2} && {cmd3} && {cmd4}");
+            res = RunCommand($"{cmd1} && {cmd2} && {cmd3} && {cmd4}"); if (res == "!con") return res;
 
             return "Комната успешно создана!";
         }
     }
+    static int Playnow = 0, Playroom = 0;
     /// <summary>
     /// Инициализация устройства в комнату.
     /// </summary>
@@ -82,46 +76,90 @@ class Web
     /// <param name="Parameters">Начальные параметры игры.</param>
     /// <param name="Names">Имена людей.</param>
     /// <returns></returns>
-    public static string InitRoom(string Room, int Players, string Parameters, string Names)
+    public static string InitRoom(string Room, int Players, string Parameters, string Names, string error = "0")
     {
-        if (Players <= 0)
-            return "Некорректное значение количества игроков";
+        switch (error)
+        {
+            case "0":
+                string res, res1, res2;
+                if (Players <= 0)
+                    return "Некорректное значение количества игроков";
 
-        if (RunCommand($"if [ -e rooms/{Room}/ ]; then echo -n 'Exists'; fi") != "Exists")
-            return "Комната не найдена";
+                goto case "!exists";
+            case "!exists":
 
-        if (RunCommand($"cat rooms/{Room}/roomStatus") == "Running")
-            return "В этой комнате идёт игра";
+                res = RunCommand($"if [ -e rooms/{Room}/ ]; then echo -n 'Exists'; fi"); if (res == "!con") return "!exists";
+                if (res != "Exists")
+                    return "Комната не найдена";
 
-        int Playnow = PlayersNow(Room), Playroom = Convert.ToInt32(RunCommand($"cat rooms/{Room}/roomPlayers"));
+                goto case "!running";
+            case "!running":
 
-        if (Playnow + Players > Playroom)
-            return "Эта комната уже заполнена";
+                res = RunCommand($"cat rooms/{Room}/roomStatus"); if (res == "!con") return "!running";
+                if (res == "Running")
+                    return "В этой комнате идёт игра";
 
-        room = Room;
-        index = new int[Players];
-        int x = Convert.ToInt32(RunCommand($"cat rooms/{Room}/index"));
+                goto case "!roomplayers";
+            case "!roomplayers":
 
-        for (int i = 0; i < Players; ++i, ++x) index[i] = x;
+                res1 = PlayersNow(Room);
+                res2 = RunCommand($"cat rooms/{Room}/roomPlayers");
+                if (res1 == "!con" || res2 == "!con") return "!roomplayers";
+                Playnow = Convert.ToInt32(res1); Playroom = Convert.ToInt32(res2);
 
-        RunCommand($"echo -n '{x}' > rooms/{Room}/index");
+                if (Playnow + Players > Playroom)
+                    return "Эта комната уже заполнена";
 
-        if (index[0] == 0) RunCommand($"echo -n '{Parameters}' > rooms/{Room}/parameters");
+                goto case "!getindex";
+            case "!getindex":
 
-        RunCommand($"echo -n '{Names}' >> rooms/{Room}/names");
+                index = new int[Players];
+                res = RunCommand($"cat rooms/{Room}/index"); if (res == "!con") return "!getindex";
+                int x = Convert.ToInt32(res);
+                for (int i = 0; i < Players; ++i, ++x) index[i] = x;
 
-        if (Playnow + Players == Playroom)
-            RunRoom(Room);
+                goto case "!newindex";
+            case "!newindex":
 
-        return $"Вы присоединились к комнате {room}";
+                res = RunCommand($"echo -n '{index[Players - 1] + 1}' > rooms/{Room}/index"); if (res == "!con") return "!newindex";
+
+                goto case "!parameters";
+            case "!parameters":
+
+                if (index[0] == 0)
+                {
+                    res = RunCommand($"echo -n '{Parameters}' > rooms/{Room}/parameters"); if (res == "!con") return "!parameters";
+                }
+
+                goto case "!names";
+            case "!names":
+
+                res = RunCommand($"echo -n '{Names}' >> rooms/{Room}/names"); if (res == "!con") return "!names";
+
+                goto case "!run";
+            case "!run":
+
+                if (Playnow + Players == Playroom)
+                {
+                    res = RunRoom(Room); if (res == "!con") return "!run";
+                }
+
+                goto default;
+            default:
+                room = Room;
+                return $"Вы присоединились к комнате {room}";
+        }
+
+
+
     }
     /// <summary>
     /// Передача строки для хранения на сервере, используется после инициализации устройства в комнату.
     /// </summary>
     /// <param name="str">Строка для сохранения на сервере.</param>
-    public static void Write(string str)
+    public static string Write(string str)
     {
-        RunCommand($"echo -n '{str}' > rooms/{room}/file");
+        return RunCommand($"echo -n '{str}' > rooms/{room}/file");
     }
     /// <summary>
     /// Получение строки, соответсвующей последнему использованию Write().
@@ -131,14 +169,19 @@ class Web
     {
         return RunCommand($"cat rooms/{room}/file");
     }
-    public static bool IsRoomFull()
+    /// <summary>
+    /// Возвращает "true", если комната полна, "false", если нет; "!con", если нет интернета.
+    /// </summary>
+    /// <returns></returns>
+    public static string IsRoomFull()
     {
-        return RunCommand($"cat rooms/{room}/roomStatus") == "Running";
+        string res = RunCommand($"cat rooms/{room}/roomStatus"); if (res == "!con") return res;
+        return RunCommand($"cat rooms/{room}/roomStatus") == "Running" ? "true" : "false";
     }
     /// <summary>
     /// Очищение комнаты после её использования.
     /// </summary>
-    public static void ClearRoom()
+    public static string ClearRoom()
     {
         string
                 cmd1 = $"echo -n '0' > rooms/{room}/index",
@@ -146,39 +189,39 @@ class Web
                 cmd3 = $"rm rooms/{room}/parameters",
                 cmd4 = $"echo -n 'Free' > rooms/{room}/roomStatus";
 
-        RunCommand($"if [ -e rooms/{room}/ ]; then {cmd1} && {cmd2} && {cmd3} && {cmd4}; fi");
+        return RunCommand($"if [ -e rooms/{room}/ ]; then {cmd1} && {cmd2} && {cmd3} && {cmd4}; fi");
     }
     /// <summary>
     /// Удаление комнаты после её использования.
     /// </summary>
-    public static void DeleteRoom()
+    public static string DeleteRoom()
     {
-        if (room != "") RunCommand($"if [ -e rooms/{room}/ ]; then rm -r rooms/{room}/; fi");
+        return room != "" ? RunCommand($"if [ -e rooms/{room}/ ]; then rm -r rooms/{room}/; fi") : "Не указана комната";
     }
     /// <summary>
     /// Удаление комнаты по названию.
     /// </summary>
-    public static void DeleteRoom(string Room)
+    public static string DeleteRoom(string Room)
     {
-        if (room != "") RunCommand($"if [ -e rooms/{Room}/ ]; then rm -r rooms/{Room}/; fi");
+        return room != "" ? RunCommand($"if [ -e rooms/{room}/ ]; then rm -r rooms/{Room}/; fi") : "Не указана комната";
     }
     /// <summary>
     /// Количество игроков в комнате.
     /// </summary>
     /// <param name="Room">Название комнаты.</param>
     /// <returns></returns>
-    public static int PlayersNow(string Room)
+    public static string PlayersNow(string Room)
     {
-        return Convert.ToInt32(RunCommand($"cat rooms/{Room}/index"));
+        return RunCommand($"cat rooms/{Room}/index");
     }
     /// <summary>
     /// Вместимость комнаты.
     /// </summary>
     /// <param name="Room">Название комнаты.</param>
     /// <returns></returns>
-    public static int RoomPlayers(string Room)
+    public static string RoomPlayers(string Room)
     {
-        return Convert.ToInt32(RunCommand($"cat rooms/{Room}/roomPlayers")); ;
+        return RunCommand($"cat rooms/{Room}/roomPlayers");
     }
     /// <summary>
     /// Параметры игры в комнате.
@@ -196,8 +239,8 @@ class Web
     {
         return RunCommand($"cat rooms/{room}/names");
     }
-    static void RunRoom(string Room)
+    static string RunRoom(string Room)
     {
-        RunCommand($"echo -n 'Running' > rooms/{Room}/roomStatus");
+        return RunCommand($"echo -n 'Running' > rooms/{Room}/roomStatus");
     }
 }
