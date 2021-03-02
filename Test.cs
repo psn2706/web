c4499600aeb2b3e260b81f9fe40d1a8a4575474a
psn2706 <sergei.petrov.com@mail.ru>
2021-03-02 18:45:53 +0300

    add Test.cs

---
+++ Test.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Test
+{
+    public static void test_message()
+    {
+        Web.room = 0;
+        Web.create(1, Web.room);
+        Web.join(1, Web.room, "nam", "par");
+        Web.rwait();
+        Web.set("hi!");
+        Console.WriteLine(Web.get());
+        Web.delete();
+    }
+}
